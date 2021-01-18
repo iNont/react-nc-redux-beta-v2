2abd3ae0ca5d88667810d97d9d77dcd4109fb892
Natchanon Charoensuk <natchanon.c@kbtg.tech>
2021-01-18 11:03:18 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'test-new-module'
+import 'test-new-module/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
